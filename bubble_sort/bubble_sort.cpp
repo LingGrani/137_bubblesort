b89647ead15e706ada4e90bd9b2ea54ca62547e3
LingGrani <luthfiangger2510@gmail.com>
2023-03-16 16:16:14 +0700

    update line 41

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -38,7 +38,7 @@ void bubblesortArray() {
 void unsorted() {
 	cout << endl;
 	cout << "=============================" << endl;
-	cout << "Element Array yang telah disusun";
+	cout << "Element Array yang telah disusun" << endl;
 	cout << "=============================" << endl;
 	for (int j = 0; j < n; j++) {
 		cout << a[j] << endl;
